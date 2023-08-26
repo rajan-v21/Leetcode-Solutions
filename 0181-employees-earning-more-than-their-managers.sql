942b33609d715f2e2d470df162d46c21340370e5
Rajan Vishwakarma <66174251+rajan-v21@users.noreply.github.com>
2023-08-26 17:11:30 +0530

    Easy - Time: 744ms (79.18%), Space: 0mb (100.00%)

--- 0181-employees-earning-more-than-their-managers.sql
+++ 0181-employees-earning-more-than-their-managers.sql
@@ -3,17 +3,18 @@ FROM Employee AS emp
 JOIN Employee AS mgr
 ON emp.managerId = mgr.id AND emp.salary > mgr.salary;
 
-/*------------------------------------------------------------------------------------------------------------------------------
+/*-----------------------------------------------------------------------------------------------------------------------------
 TC: O(n) SC: O(1)
 
 Intuition:
 
 Simply ask question 'What we need?', 'From where we need?' & 'How can we retrieve it?'.
-1. Using SELECT clause get the ids of the data we need but in this question we have to return the name id of table with new id 
-i.e 'Employee'.
-2. Here we have intialized Employee table as emp and mgr separately to avoid confusion and treating same table as different.
+1. Using SELECT clause get the ids of the data we need but in this question we have to return the 'name' id of the table as 
+'Employee'.
+2. Here we have intialized Employee table as emp and mgr separately to avoid confusion and treating the same table as 
+different entities.
 3. Now using FROM & JOIN we determine which tables we need to join and retrieve data.
 (JOIN : Returns records that have matching values in both tables.)
-4. And lastly, we retrieve data using ON clause providing a pseudo foreign key i.e common id present in both tables with given
-condition of question using AND clause.
-------------------------------------------------------------------------------------------------------------------------------*/
+4. And lastly, we retrieve data using ON clause providing a pseudo foreign key i.e a common id present in both the tables with 
+given condition of the question using AND clause.
+-----------------------------------------------------------------------------------------------------------------------------*/
