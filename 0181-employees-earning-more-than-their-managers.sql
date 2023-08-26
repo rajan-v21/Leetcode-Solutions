49d110d5db228a74c40748d0241ef49375d95f87
Rajan Vishwakarma <66174251+rajan-v21@users.noreply.github.com>
2023-08-26 17:04:40 +0530

    Easy - Time: 744ms (79.18%), Space: 0mb (100.00%)

--- 0181-employees-earning-more-than-their-managers.sql
+++ 0181-employees-earning-more-than-their-managers.sql
@@ -9,8 +9,8 @@ TC: O(n) SC: O(1)
 Intuition:
 
 Simply ask question 'What we need?', 'From where we need?' & 'How can we retrieve it?'.
-1. Using SELECT clause get the ids of the data we need but in this question we have to return the name table with new id i.e
-'Employee'.
+1. Using SELECT clause get the ids of the data we need but in this question we have to return the name id of table with new id 
+i.e 'Employee'.
 2. Here we have intialized Employee table as emp and mgr separately to avoid confusion and treating same table as different.
 3. Now using FROM & JOIN we determine which tables we need to join and retrieve data.
 (JOIN : Returns records that have matching values in both tables.)
