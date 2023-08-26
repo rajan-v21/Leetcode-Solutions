175bcbbfcd4b47bc6f7d0a9bc58d1b6dd3929f74
Rajan Vishwakarma <66174251+rajan-v21@users.noreply.github.com>
2023-08-26 16:44:29 +0530

    Easy - Time: 866ms (64.63%), Space: 0mb (100.00%)

--- 0175-combine-two-tables.sql
+++ 0175-combine-two-tables.sql
@@ -11,6 +11,6 @@ Intuition:
 Simply ask question 'What we need?', 'Which table we need it?' & 'How we retrieve it?'.
 1. Using SELECT clause get the ids of the data we need.
 2. Now using FROM & LEFT JOIN we determine which tables we need to join.
-(LEFT JOIN takes all the data from left table and then combines the right table's data to its left table's counterpart data.)
+(LEFT (OUTER) JOIN : Returns all records from the left table, and the matched records from the right table.)
 3. And lastly, we join both the tables using ON clause providing a foreign key i.e common id present in both tables.
 ------------------------------------------------------------------------------------------------------------------------------*/
