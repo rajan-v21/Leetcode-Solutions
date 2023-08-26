ea6b67a135b399864c6703859b69bb1a7558a1d6
Rajan Vishwakarma <66174251+rajan-v21@users.noreply.github.com>
2023-08-26 12:33:04 +0530

    Easy - Time: 942ms (45.43%), Space: 0mb (100.00%)

---
+++ 0175-combine-two-tables.sql
@@ -0,0 +1,16 @@
+SELECT firstname, lastname, city, state
+FROM Person 
+LEFT JOIN Address
+USING (personId);
+
+/*------------------------------------------------------------------------------------------------------------------------------
+TC: O(n) SC: O(1)
+
+Intuition:
+
+Simply ask question 'What we need?', 'Which table we need it?' & 'How we retrieve it?'.
+1. Using SELECT clause get the ids of the data we need.
+2. Now using FROM & LEFT JOIN we determine which tables we need to join.
+(LEFT JOIN takes all the data from left table and then combines the right table's data to its left table's counterpart data.)
+3. And lastly, we join both the tables using USING clause providing a foreign key i.e common id present in both tables.
+------------------------------------------------------------------------------------------------------------------------------*/
