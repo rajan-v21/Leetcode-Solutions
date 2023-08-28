409e5f205829ba3131dd53c011925978660de4fb
Rajan Vishwakarma <66174251+rajan-v21@users.noreply.github.com>
2023-08-28 17:08:53 +0530

    Easy - Time: 866ms (64.63%), Space: 0mb (100.00%)

--- 0175-combine-two-tables.sql
+++ 0175-combine-two-tables.sql
@@ -1,10 +1,18 @@
+CREATE TABLE Person (personId INT, lastname VARCHAR(10), firstname VARCHAR(10));
+INSERT INTO Person 
+VALUES ROW(1, Wang, Allen), ROW(2, Alice, Bob);
+
+CREATE TABLE Address (addressId INT, personId INT, city VARCHAR(10), state VARCHAR(10));
+INSERT INTO Address 
+VALUES ROW(1, 2, New York City, New York), ROW(2, 3, Leetcode, California);
+
 SELECT firstname, lastname, city, state
 FROM Person 
 LEFT JOIN Address
 ON Person.personId = Address.personId;
 
 /*------------------------------------------------------------------------------------------------------------------------------
-TC: O(n + m) SC: O(1)
+TC: O(n + m) SC: O(1) [for running query]
 
 Intuition:
 
