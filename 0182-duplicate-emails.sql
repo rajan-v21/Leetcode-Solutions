76aa97d482057cf75a3e2054be5c4413dd48d312
Rajan Vishwakarma <66174251+rajan-v21@users.noreply.github.com>
2023-08-28 16:56:38 +0530

    Easy - Time: 807ms (60.94%), Space: 0mb (100.00%)

--- 0182-duplicate-emails.sql
+++ 0182-duplicate-emails.sql
@@ -1,12 +1,16 @@
+CREATE TABLE Person (id INT, email VARCHAR(20));
+INSERT INTO Person 
+VALUES ROW(1, a@b.com), ROW(2, c@d.com), ROW(3, a@b.com);
+
 SELECT Email
 From Person
 GROUP BY email
 HAVING COUNT(*) > 1;
 
 /*--------------------------------------------------------------------------------------------------------------------------------
-TC: O(nlog(n)) SC: O(1)
+TC: O(nlog(n)) SC: O(1) [for running query]
 
 Intuition:
 
-1. Simply select the emails from person table and then group the emails alphabetically which are having count more than 1.
+1. Simply select the emails from person table and then group the emails alphabetically which are occuring more than once.
 --------------------------------------------------------------------------------------------------------------------------------*/
